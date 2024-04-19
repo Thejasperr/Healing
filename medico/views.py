7f32a2aeb39aaf3bc979e20a95d5e020bbff46f3
Thejasperr <joaoizidrosilva@gmail.com>
2024-04-19 11:54:14 -0300

    Adicionado Navbar

--- medico/views.py
+++ medico/views.py
@@ -16,7 +16,7 @@ def cadastro_medico(request):
 
     if request.method == "GET":
         especialidades = Especialidades.objects.all()
-        return render(request, 'cadastro_medico.html', {'especialidades': especialidades})
+        return render(request, 'cadastro_medico.html', {'especialidades': especialidades, 'is_medico': is_medico(request.user)})
     elif request.method == "POST":
         crm = request.POST.get('crm')
         nome = request.POST.get('nome')
@@ -60,7 +60,7 @@ def abrir_horario(request):
     if request.method == "GET":
         dados_medicos = DadosMedico.objects.get(user=request.user)
         datas_abertas = DatasAbertas.objects.filter(user=request.user)
-        return render(request, 'abrir_horario.html', {'dados_medicos':dados_medicos, 'datas_abertas':datas_abertas})
+        return render(request, 'abrir_horario.html', {'dados_medicos':dados_medicos, 'datas_abertas':datas_abertas, 'is_medico': is_medico(request.user)})
     elif request.method == "POST":
         data = request.POST.get('data')
         data_formatada = datetime.strptime(data, '%Y-%m-%dT%H:%M')
@@ -84,4 +84,4 @@ def consultas_medico(request):
     consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1))
     consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id'))
 
-    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes})
+    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
